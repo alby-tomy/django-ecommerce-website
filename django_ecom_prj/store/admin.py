f4de5901a95183e7d552e68d4d08f2b92ee273ee
alby-tomy <alby.u.tomy@gmail.com>
2024-03-15 06:20:40 +0530

    models creation order, product, customer, shipping, order-items

--- django_ecom_prj/store/admin.py
+++ django_ecom_prj/store/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Order, OrderItem, Product, Customer, ShippingAddress
 
 # Register your models here.
+
+admin.site.register(Order)
+admin.site.register(Product)
+admin.site.register(Customer)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
+
